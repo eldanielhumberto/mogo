50b45cf17dc4b6d157765c1a9c0403b1d2902130
eldanielhumberto <gamekik92@gmail.com>
2026-01-29 12:17:04 -0600

    feat: add init command with generate mogo.json

--- cmd/init.go
+++ cmd/init.go
@@ -1,15 +1,45 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
+	"github.com/eldanielhumberto/mogo/internal/helpers"
 	"github.com/spf13/cobra"
 )
+
+type Settings struct {
+	Commands map[string]any `json:"commands"`
+}
 
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Create mogo.json file",
 	Run: func(cmd *cobra.Command, args []string) {
+		filename := "mogo.json"
+		exists := helpers.CheckFileExists(filename)
+
+		if exists {
+			fmt.Printf("File %s already exists\n", filename)
+			return
+		}
+
+		settings := Settings{
+			Commands: map[string]any{},
+		}
+		jsonData, err := json.MarshalIndent(settings, "", "  ")
+		if err != nil {
+			fmt.Printf("Error marshalling to JSON: %v\n", err)
+			return
+		}
+
+		err = os.WriteFile("mogo.json", jsonData, 0644)
+		if err != nil {
+			fmt.Printf("Error writing to file: %v\n", err)
+			return
+		}
+
 		fmt.Println("init called")
 	},
 }
